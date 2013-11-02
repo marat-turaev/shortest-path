de5e6238560911a66b9eec2a52ee8cc51c0c9f80
Marat Turaev <marat.turaev@hotmail.com>
2013-11-02 23:14:09 +0400

    fixed .gitignore

--- main.cpp
+++ main.cpp
@@ -6,5 +6,5 @@ int main() {
 	// graph* g = graph::construct_from_file("input/light.co", "input/light.gr");
 	// g->print(std::cout);
 	std::cout.precision(15);
-	std::cout<<dijkstra(g).shortest_path(1, 50000)<<std::endl;
+	std::cout<<dijkstra(g).shortest_path(0, 50000)<<std::endl;
 }
